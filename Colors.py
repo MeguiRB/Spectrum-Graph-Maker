b6cab69a2808005911139f313f3e0d5b03426a0e
MeguiRB <87879934+MeguiRB@users.noreply.github.com>
2022-03-09 16:08:04 +0000

    Put dictionary in file

---
+++ Colors.py
@@ -0,0 +1,17 @@
+color_dictionary = {"light blue": "tab:blue",
+                    "orange": "tab:orange",
+                    "green": "tab:green",
+                    "dark red": "tab:red",
+                    "purple": "tab:purple",
+                    "brown": "tab:brown",
+                    "pink": "tab:pink",
+                    "grey": "tab:gray",
+                    "olive": "tab:olive",
+                    "cyan": "tab:cyan",
+                    "dark blue": "b",
+                    "dark green": "g",
+                    "red": "r",
+                    "magenta": "m",
+                    "black": "k",
+                    "white": "w"
+                    }
